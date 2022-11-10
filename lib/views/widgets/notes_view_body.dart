2e2d4d208e8f9ada21180b10f37890797ed47f3e
reham611 <r.ramadan.ict@btu.edu.eg>
2022-11-10 12:03:37 -0800

    complete the proj

---
+++ lib/views/widgets/notes_view_body.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/views/widgets/custom_app_bar.dart';
+import 'custom_app_bar.dart';
+import 'custom_note_item.dart';
+class NotesViewBody extends StatelessWidget {
+  const NotesViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
+        children: const [
+          SizedBox(
+            height: 50,
+          ),
+          CustomAppBar(),
+          NoteItem(),
+
+        ],
+      ),
+    );
+  }
+}
